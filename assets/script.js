1dc730cbd8a0c176d1550bcec6659dd142b8a3dd
Joey Scott <joannascott29@hotmail.co.uk>
2016-11-14 22:36:44 +0000

    Add list-responsive to nav

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
 var d = new Date();
 document.getElementById("date-time").innerHTML = d.toDateString();
 
-//var hero = document.getElementById('hero'),
-   var leaves = document.getElementById("leaves"),
-    viewportHeight = Math.max(window.innerHeight || 0),
-    leavesOffsetTop = leaves.offsetTop;
+// //var hero = document.getElementById('hero'),
+//    var leaves = document.getElementById("leaves"),
+//     viewportHeight = Math.max(window.innerHeight || 0),
+//     leavesOffsetTop = leaves.offsetTop;
 
-//hero.style.height = viewportHeight + 'px';
+// //hero.style.height = viewportHeight + 'px';
 
-var hasScrolled = function() {
+// var hasScrolled = function() {
 
-    var fromTop = document.body.scrollTop || document.documentElement.scrollTop || 0;
+//     var fromTop = document.body.scrollTop || document.documentElement.scrollTop || 0;
 
-    if (leavesOffsetTop < fromTop) {
-        leaves.classList.add('fixed-position');
-    } else {
-        leaves.classList.remove('fixed-position');
-    }
+//     if (leavesOffsetTop < fromTop) {
+//         leaves.classList.add('fixed-position');
+//     } else {
+//         leaves.classList.remove('fixed-position');
+//     }
 
-}
+// }
 
-window.addEventListener('scroll', hasScrolled);
+// window.addEventListener('scroll', hasScrolled);
