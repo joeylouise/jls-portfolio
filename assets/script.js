044a21f1a44e3044e3e2429a116208c1d9464361
Joey Scott <joannascott29@hotmail.co.uk>
2016-11-27 17:47:41 +0000

    Clean up contact section

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,6 @@
 var d = new Date();
 document.getElementById("date-time").innerHTML = d.toDateString();
 
-// //var hero = document.getElementById('hero'),
-//    var leaves = document.getElementById("leaves"),
-//     viewportHeight = Math.max(window.innerHeight || 0),
-//     leavesOffsetTop = leaves.offsetTop;
-
-// //hero.style.height = viewportHeight + 'px';
-
-// var hasScrolled = function() {
-
-//     var fromTop = document.body.scrollTop || document.documentElement.scrollTop || 0;
-
-//     if (leavesOffsetTop < fromTop) {
-//         leaves.classList.add('fixed-position');
-//     } else {
-//         leaves.classList.remove('fixed-position');
-//     }
-
-// }
-
-// window.addEventListener('scroll', hasScrolled);
-
 function showNav(target){
 	document.getElementById(target).style.display = 'block';
 	document.getElementById("site-nav__cross").style.display = 'block';
@@ -44,3 +23,26 @@ function restoreDisplay() {
 		document.getElementById("site-nav__hamburger").style.display = 'block';
 	}
 }
+
+//POTENTIALLY ADD IN POST-MVP - STICKY LEAVES
+
+// //var hero = document.getElementById('hero'),
+//    var leaves = document.getElementById("leaves"),
+//     viewportHeight = Math.max(window.innerHeight || 0),
+//     leavesOffsetTop = leaves.offsetTop;
+
+// //hero.style.height = viewportHeight + 'px';
+
+// var hasScrolled = function() {
+
+//     var fromTop = document.body.scrollTop || document.documentElement.scrollTop || 0;
+
+//     if (leavesOffsetTop < fromTop) {
+//         leaves.classList.add('fixed-position');
+//     } else {
+//         leaves.classList.remove('fixed-position');
+//     }
+
+// }
+
+// window.addEventListener('scroll', hasScrolled);
